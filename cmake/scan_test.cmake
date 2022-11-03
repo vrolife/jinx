d5fdaac488204fcb4a7342a3ec5c9d71ea7ffd53
pom <pom@vro.life>
2022-11-03 14:49:47 +0800

    [Test] Improve jinx_scan_test

---
+++ cmake/scan_test.cmake
@@ -0,0 +1,32 @@
+
+function(_add_jinx_test)
+    cmake_parse_arguments(TEST "" "SOURCE" "SOURCES;LIBRARIES" ${ARGN})
+    
+    string(REPLACE .cpp "" TARGET_NAME "${TEST_SOURCE}")
+
+    add_executable(${TARGET_NAME} ${TEST_SOURCE} ${TEST_SOURCES})
+    target_link_libraries(${TARGET_NAME} PRIVATE ${TEST_LIBRARIES})
+    target_compile_definitions(${TARGET_NAME} PRIVATE -DJINX_FORCE_ASSERT=1)
+    
+    if (EXISTS "${TARGET_NAME}.cmake")
+        include("${TARGET_NAME}.cmake")
+    endif()
+
+    add_test(NAME ${TARGET_NAME} COMMAND $<TARGET_FILE:${TARGET_NAME}>)
+endfunction(_add_jinx_test)
+
+function(jinx_scan_test)
+    cmake_parse_arguments(TEST "" "" "SOURCES;LIBRARIES" ${ARGN})
+
+    file(GLOB_RECURSE SOURCE_FILE_LIST LIST_DIRECTORIES false RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}/ test_*.cpp)
+
+    foreach(SOURCE_FILE ${SOURCE_FILE_LIST})
+        if (JINX_TEST_WHITE_LIST)
+            if (SOURCE_FILE IN_LIST JINX_TEST_WHITE_LIST)
+                _add_jinx_test(SOURCE ${SOURCE_FILE} SOURCES ${TEST_SOURCES} LIBRARIES ${TEST_LIBRARIES})
+            endif()
+        else()
+            _add_jinx_test(SOURCE ${SOURCE_FILE} SOURCES ${TEST_SOURCES} LIBRARIES ${TEST_LIBRARIES})
+        endif()
+    endforeach()
+endfunction()
